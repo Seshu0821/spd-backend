a49a96b16e4aee82a1256a02c5924d97eac7e79b
Seshu0821 <Seshug0811@gmail.com>
2026-03-10 16:14:18 +0530

    Enhance prediction with SHAP and performance metrics
    Added SHAP explainer and performance evaluation to predictions.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.compose import ColumnTransformer
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from pydantic import BaseModel
+import shap
 
 
 class PredictionInput(BaseModel):
@@ -22,14 +23,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 model = None
 preprocessor = None
+explainer = None
+feature_names = None
 
 
 @app.get("/")
@@ -39,7 +42,7 @@ def read_root():
 
 @app.on_event("startup")
 def load_model():
-    global model, preprocessor
+    global model, preprocessor, explainer, feature_names
 
     df = pd.read_csv("data/stud.csv")
 
@@ -59,10 +62,15 @@ def load_model():
         ]
     )
 
-    X = preprocessor.fit_transform(X)
+    X_transformed = preprocessor.fit_transform(X)
 
     model = LinearRegression()
-    model.fit(X, y)
+    model.fit(X_transformed, y)
+
+    # SHAP explainer
+    explainer = shap.LinearExplainer(model, X_transformed)
+
+    feature_names = preprocessor.get_feature_names_out()
 
     print("Model loaded successfully")
 
@@ -76,6 +84,48 @@ def predict(data: PredictionInput):
 
     prediction = model.predict(input_processed)
 
+    score = float(prediction[0])
+
+    # Pass / Fail
+    status = "Pass" if score >= 40 else "Fail"
+
+    # Performance Level
+    if score < 40:
+        performance = "Very Weak"
+    elif score < 50:
+        performance = "Poor"
+    elif score < 60:
+        performance = "Average"
+    elif score < 75:
+        performance = "Good"
+    else:
+        performance = "Excellent"
+
+    # SHAP explanation
+    shap_values = explainer(input_processed)
+
+    contributions = {}
+
+    for name, val in zip(feature_names, shap_values.values[0]):
+        clean_name = name.split("__")[-1]
+        contributions[clean_name] = float(val)
+
+    # Important features for UI
+    important_features = [
+        "reading_score",
+        "writing_score",
+        "test_preparation_course",
+        "parental_level_of_education"
+    ]
+
+    filtered_contributions = {
+        k: v for k, v in contributions.items()
+        if any(feature in k for feature in important_features)
+    }
+
     return {
-        "predicted_math_score": float(prediction[0])
+        "predicted_math_score": score,
+        "status": status,
+        "performance_level": performance,
+        "feature_contributions": filtered_contributions
     }
